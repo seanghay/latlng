e75f30c136215fd20b4640f801dd82dd2f0f19fc
Yath Seanghay <seanghay.dev@gmail.com>
2022-03-03 16:05:16 +0700

    add copy button

--- main.js
+++ main.js
@@ -1,38 +1,51 @@
 import "normalize.css";
 import "./style.scss";
+import copy from "clipboard-copy";
 
 const buttonRequest = document.getElementById("button-request");
 const inputRequest = document.getElementById("input-location");
+const buttonCopy = document.getElementById("button-copy");
+
+buttonCopy.addEventListener("click", async () => {
+  buttonCopy.disabled = true;
+  await copy(inputRequest.value);
+  buttonCopy.textContent = "Copied";
+  setTimeout(() => {
+    buttonCopy.textContent = "Copy";
+    buttonCopy.disabled = false;
+  }, 1000);
+});
 
 document.addEventListener("DOMContentLoaded", () => {
   document.body.classList.remove("loading");
   buttonRequest.addEventListener("click", () => {
     loading(true);
-    navigator.geolocation.getCurrentPosition((e) => {
-      const {latitude, longitude} = e.coords
-      inputRequest.value = `${latitude}, ${longitude}`;
-      loading(false);
-    }, error => {
-      loading(false);
-      console.error(error);
-      const { message } = error;
-      inputRequest.value = message || 'Error';
-    }, { 
-      enableHighAccuracy: true,
-    })
+    navigator.geolocation.getCurrentPosition(
+      (e) => {
+        const { latitude, longitude } = e.coords;
+        inputRequest.value = `${latitude}, ${longitude}`;
+        loading(false);
+      },
+      (error) => {
+        loading(false);
+        console.error(error);
+        const { message } = error;
+        inputRequest.value = message || "Error";
+      },
+      {
+        enableHighAccuracy: true,
+      }
+    );
   });
-
 });
 
-
 function loading(isLoading) {
-
   if (isLoading) {
     buttonRequest.disabled = true;
-    buttonRequest.textContent = 'Loading…';
+    buttonRequest.textContent = "Loading…";
     return;
   }
 
   buttonRequest.disabled = false;
-  buttonRequest.textContent = 'Request Current Location';
+  buttonRequest.textContent = "Request Current Location";
 }
