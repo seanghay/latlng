3c2d0cfdccca9d8e01e47246df112f08cf5cf85c
Yath Seanghay <seanghay.dev@gmail.com>
2022-06-16 16:38:50 +0700

    feat: add histories

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,26 @@
 import { Codemirror } from "vue-codemirror";
 import { json } from "@codemirror/lang-json";
 import { oneDark } from "@codemirror/theme-one-dark";
-
+import { nanoid } from "nanoid";
 import { computed } from "@vue/reactivity";
 import { effect } from "vue";
 import { ref } from "vue";
 import Button from "./components/Button.vue";
 import { requestCurrentLocation } from "./location";
 import copy from "clipboard-copy";
+import { formatDistance, parseISO } from "date-fns";
 
 //@ts-ignore
 import { OpenLocationCode } from "open-location-code";
 import ClipboardButton from "./components/ClipboardButton.vue";
 
 const openLocationCode = new OpenLocationCode();
+const histories = ref<any[]>([]);
+const historyVisible = ref<boolean>(false);
+const activeSelectionId = ref <string | null>(null);
 
 const isCopyToClipboard = ref(false);
-const isJsonViewVisible = ref(true);
+const isJsonViewVisible = ref(false);
 const isLoading = ref(false);
 const results = ref<any[]>([]);
 const hasResult = computed(() => results.value && results.value.length);
@@ -29,7 +33,15 @@ const jsonValue = computed(() =>
   )
 );
 
-effect(async () => {});
+effect(async () => {
+  await invalidateHistories()
+});
+
+
+async function invalidateHistories() {
+  const db = await cacheDb();
+  histories.value = db as any[];
+}
 
 const extensions = [json(), oneDark];
 
@@ -65,8 +77,35 @@ async function reverseGeo({ latitude, longitude }: any) {
   }
 }
 
+async function cacheDb() {
+  return new Promise((resolve) => {
+    const key = `__items__`;
+    const db = JSON.parse(localStorage.getItem(key) || `[]`);
+    resolve(
+      db.map((item: any) => ({
+        ...item,
+        data: Object.fromEntries(item.items.map((i: any) => [i.id, i.value])),
+      }))
+    );
+  });
+}
+
+async function persist(items: any[]) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const id = nanoid();
+      const key = `__items__`;
+      const db = JSON.parse(localStorage.getItem(key) || `[]`);
+      const newDb = [{ id, items, createdAt: new Date().toISOString() }, ...db].slice(0, 20);
+      localStorage.setItem(key, JSON.stringify(newDb));
+      resolve(items);
+    });
+  });
+}
+
 async function requestLocation() {
   isLoading.value = true;
+  activeSelectionId.value = null;
   const { coords } = await requestCurrentLocation();
   const { latitude, longitude } = coords;
 
@@ -88,13 +127,20 @@ async function requestLocation() {
 
   isLoading.value = false;
   const data = await reverseGeo({ latitude, longitude });
-  const append = Object.entries(data.address).map(([key, value]) => ({
+  let append = Object.entries(data.address).map(([key, value]) => ({
     id: key,
     label: getOrDefault(key, () => key),
     value,
   }));
 
+  append = [
+    { id: "display_name", label: "Address", value: data.display_name },
+    ...append,
+  ];
+
   results.value = [...first, ...append];
+  await persist(results.value);
+  await invalidateHistories()
 }
 
 function toggleJSONViewer() {
@@ -107,6 +153,24 @@ async function copyClipboard() {
   setTimeout(async () => {
     isCopyToClipboard.value = false;
   }, 1000);
+}
+
+function showHistory() {
+  historyVisible.value = !historyVisible.value;
+}
+
+function formatDate(str: string) {
+  return formatDistance(parseISO(str), new Date(), { addSuffix: true });
+}
+
+function selectItem({ items, id }: any) {
+  activeSelectionId.value = id;
+  results.value = items;
+}
+
+function clearAll() {
+  localStorage.removeItem('__items__');
+  histories.value = []
 }
 </script>
 
@@ -135,9 +199,11 @@ async function copyClipboard() {
       Get detailed information about your current location.
     </p>
     <div class="mt-4">
-      <Button :loading="isLoading" block @click="requestLocation"
-        >Request</Button
-      >
+      <Button :loading="isLoading" block @click="requestLocation">
+        <div class="text-xl inline-block align-top i-ri-play-circle-line"></div>
+
+        <span class="mx-1">Request</span>
+      </Button>
     </div>
 
     <div class="mt-4">
@@ -155,22 +221,20 @@ async function copyClipboard() {
         <h1 v-if="item.label" class="font-semibold opacity-60 text-sm">
           {{ item.label }}
         </h1>
-        <h1 v-if="item.value" class="text-white">{{ item.value }}</h1>
-        <ClipboardButton :inputValue="item.value"/>
+        <h1 v-if="item.value" class="text-white pr-10">{{ item.value }}</h1>
+        <ClipboardButton v-if="item.value" :inputValue="item.value" />
       </div>
     </div>
 
     <Button v-if="hasResult" @click="toggleJSONViewer" class="mt-4 mr-2" small>
       <template v-if="!isJsonViewVisible">
         <div
-          class="text-xl inline-block align-top i-ri-arrow-drop-down-line"
+          class="text-xl inline-block align-top i-ri-arrow-down-s-fill"
         ></div>
         <span class="px-2 inline-block">View JSON</span>
       </template>
       <template v-else>
-        <div
-          class="text-xl inline-block align-top i-ri-arrow-drop-up-line"
-        ></div>
+        <div class="text-xl inline-block align-top i-ri-arrow-up-s-fill"></div>
         <span class="px-2 inline-block">Hide JSON</span>
       </template>
     </Button>
@@ -204,6 +268,54 @@ async function copyClipboard() {
         :model-value="jsonValue"
         :extensions="extensions"
       ></codemirror>
+    </div>
+
+    <div class="mt-4">
+      <Button small block @click="showHistory()">
+        <div
+          class="text-xl inline-block align-top"
+          :class="[
+            historyVisible ? 'i-ri-arrow-up-s-fill' : 'i-ri-arrow-down-s-fill',
+          ]"
+        ></div>
+        <span v-if="historyVisible" class="mx-1">Hide History</span>
+        <span v-else class="mx-1">Show History</span>
+      </Button>
+    </div>
+
+    <div class="mt-4 pb-10" v-if="historyVisible">
+      <h1 class="text-2xl font-bold">
+        <div class="text-2xl inline-block align-middle i-ri-history-fill"></div>
+        <span class="mx-2">History</span>
+      </h1>
+
+      <div class="flex flex-col gap-2 mt-2">
+
+        <p v-if="histories.length == 0">Empty</p>
+
+        <div
+          @click="selectItem(item)"
+          tabindex="1"
+        
+          class="focus:outline-light-600 active:bg-dark-500 border cursor-pointer rounded bg-dark-400 border-dark-300 px-3 py-2"
+          :class="[activeSelectionId === item.id ? 'border-gray-200 text-black bg-gray-300' : null]"
+          v-for="item in histories"
+          :key="item.id"
+        >
+          <h1 class="font-bold m-0 p-0">{{ item.data.position }}</h1>
+          <h4 class="text-xs opacity-80">{{ item.data.display_name }}</h4>
+          <h6
+            class="mt-2 text-white bg-dark-300 px-2 rounded mb-1 py-1 inline-block text-xs font-bold"
+          >
+            {{ formatDate(item.createdAt) }}
+          </h6>
+        </div>
+      </div>
+
+      <Button v-if="histories.length" @click="clearAll()" small class="mt-4 mr-2">
+        <div class="text-xl inline-block align-top i-ri-delete-bin-7-line"></div>
+        <span class=" px-2 inline-block">Clear all</span>
+      </Button>
     </div>
   </div>
 </template>
